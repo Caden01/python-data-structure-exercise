6f9bdfaca0968f5a0d8b4e9edab6edbf5252b645
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2023-12-05 11:21:54 -0800

    Add function that checks if two tuples have anything in common

--- 19_friend_date/friend_date.py
+++ 19_friend_date/friend_date.py
@@ -16,3 +16,12 @@ def friend_date(a, b):
         >>> friend_date(sauron, gandalf)
         True
     """
+
+    return bool(set(a[2]) & set(b[2]))
+
+elmo = ('Elmo', 5, ['hugging', 'being nice'])
+sauron = ('Sauron', 5000, ['killing hobbits', 'chess'])
+gandalf = ('Gandalf', 10000, ['waving wands', 'chess'])
+
+print(friend_date(elmo, sauron))
+print(friend_date(gandalf, sauron))
