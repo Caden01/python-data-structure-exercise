dfc137f89c387efa9d81ed48b0cb7d0b4272104a
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2023-12-05 07:14:09 -0800

    Add function that returns the day of the week

--- 02_weekday_name/weekday_name.py
+++ 02_weekday_name/weekday_name.py
@@ -12,3 +12,12 @@ def weekday_name(day_of_week):
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    
+    days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] 
+
+    if 1 < day_of_week > 7 :
+        return None
+    else :
+        return days[day_of_week - 1]
+
+print(weekday_name(10))
