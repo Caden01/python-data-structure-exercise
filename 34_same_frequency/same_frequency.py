94bf029290e69d51d6f24e8b98bfd9d0fe421036
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2023-12-06 12:20:31 -0800

    Add function that checks if two numbers have the same frequency of numbers

--- 34_same_frequency/same_frequency.py
+++ 34_same_frequency/same_frequency.py
@@ -10,3 +10,15 @@ def same_frequency(num1, num2):
         >>> same_frequency(1212, 2211)
         True
     """
+
+    num1 = str(num1)
+    num2 = str(num2)
+    
+    freq1 = {num: num1.count(num) for num in num1} 
+    freq2 = {num: num2.count(num) for num in num2}
+
+    return freq1 == freq2
+
+print(same_frequency(551122, 221515))
+print(same_frequency(321142, 3212215))
+print(same_frequency(1212, 2211))
