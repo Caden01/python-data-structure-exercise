98759340d03847bd895bd582e2e44b192a5ef4c3
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2023-12-06 13:19:15 -0800

    Add function that checks if the parentheses are validly placed

--- fs_2_valid_parentheses/valid_parentheses.py
+++ fs_2_valid_parentheses/valid_parentheses.py
@@ -22,3 +22,13 @@ def valid_parentheses(parens):
         >>> valid_parentheses(")()(")
         False
     """
+
+    count = {paren: parens.count(paren) for paren in parens}
+    temp = 0
+    for val in count.values() :
+        if temp == val : 
+            return True
+        temp = val
+    return False
+print(valid_parentheses("(()())"))
+print(valid_parentheses("((())"))
