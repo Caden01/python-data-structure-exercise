f80c8bedf6028fca7b82f41050f46f163042ae47
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2023-12-06 08:50:29 -0800

    Add function that find all factors of number given

--- 28_find_factors/find_factors.py
+++ 28_find_factors/find_factors.py
@@ -13,3 +13,11 @@ def find_factors(num):
     >>> find_factors(321421)
     [1, 293, 1097, 321421]
     """
+
+    lst = [i for i in range(1, num // 2 + 1) if num % i == 0]
+
+    lst.append(num)
+
+    return lst
+
+print(find_factors(321421))
