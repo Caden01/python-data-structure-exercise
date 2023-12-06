2f69f0678deacf1d1afac94082f9c050612d2088
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2023-12-06 12:42:23 -0800

    Add function that finds the duplicate in a list

--- 36_find_the_duplicate/find_the_duplicate.py
+++ 36_find_the_duplicate/find_the_duplicate.py
@@ -13,3 +13,14 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+
+
+    count = {num: nums.count(num) for num in nums}
+    
+    for key, val in count.items() :
+        if val > 1 :
+            return key
+
+print(find_the_duplicate([1, 2, 1, 4, 3, 12]))
+print(find_the_duplicate([6, 1, 9, 5, 3, 4, 9]))
+print(find_the_duplicate([2, 1, 3, 4]) is None)
